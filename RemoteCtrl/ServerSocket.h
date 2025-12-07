ef440521cf67ca24c5891d50cc957249e2e76849
wzy <wzy@163.com>
2025-12-07 12:04:07 +0800

    完成了鼠标消息的处理

--- RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/ServerSocket.h
@@ -160,6 +160,19 @@ public:
 };
 #pragma pack(pop)
 
+//鼠标事件数据结构
+typedef struct MouseEvent {
+	MouseEvent()
+	{
+		nAction = 0;
+		nButton = -1;
+		ptXY.x = 0;
+		ptXY.y = 0;
+	}
+	WORD nAction;//点击、移动、双击
+	WORD nButton;//左键、右键、中键 0\2\1
+	POINT ptXY;//坐标
+}MOUSEEVENT,*PMOUSEEVENT;
 
 //网络服务类，单例模式
 class CServerSocket
@@ -249,6 +262,15 @@ public:
 	{
 		if ((m_packet.sCmd == 2) || (m_packet.sCmd == 3) || (m_packet.sCmd == 4)) {//判断当前命令是否为处理文件的命令
 			strPath = m_packet.strData;
+			return true;
+		}
+		return false;
+	}
+
+	bool GetMouseEvent(MOUSEEVENT& mouse)
+	{
+		if (m_packet.sCmd == 5) {
+			memcpy(&mouse, m_packet.strData.c_str(), sizeof(MOUSEEVENT));
 			return true;
 		}
 		return false;
