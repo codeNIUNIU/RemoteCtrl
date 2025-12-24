97c54b7b418b5323f1d57f78a25bb0da083c4280
wzy <wzy@163.com>
2025-12-24 09:11:23 +0800

    完成远程桌面的显示

--- RemotClient/WatchDialog.cpp
+++ RemotClient/WatchDialog.cpp
@@ -31,11 +31,27 @@ void CWatchDialog::DoDataExchange(CDataExchange* pDX)
 
 BEGIN_MESSAGE_MAP(CWatchDialog, CDialog)
 	ON_WM_TIMER()
+	ON_WM_LBUTTONDBLCLK()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_RBUTTONDBLCLK()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_MOUSEMOVE()
 END_MESSAGE_MAP()
 
 
 // CWatchDialog 消息处理程序
 
+
+CPoint CWatchDialog::UserPoint2RemoteScreenPoint(CPoint& point)
+{
+	CRect clientRect;
+	ScreenToClient(&point);//将用户点转换为客户端点,全局坐标转换为客户区域坐标
+	m_picture.GetWindowRect(&clientRect);//获取图片控件的客户区域矩形
+
+	return CPoint(point.x * 1920 / clientRect.Width(), point.y * 1080 / clientRect.Height());//将用户点转换为远程屏幕点
+}
 
 BOOL CWatchDialog::OnInitDialog()
 {
@@ -67,4 +83,60 @@ void CWatchDialog::OnTimer(UINT_PTR nIDEvent)
 		}
 	}
 	CDialog::OnTimer(nIDEvent);
+}
+
+
+void CWatchDialog::OnLButtonDblClk(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialog::OnLButtonDblClk(nFlags, point);
+}
+
+
+void CWatchDialog::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialog::OnLButtonDown(nFlags, point);
+}
+
+
+void CWatchDialog::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialog::OnLButtonUp(nFlags, point);
+}
+
+
+void CWatchDialog::OnRButtonDblClk(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialog::OnRButtonDblClk(nFlags, point);
+}
+
+
+void CWatchDialog::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialog::OnRButtonDown(nFlags, point);
+}
+
+
+void CWatchDialog::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialog::OnRButtonUp(nFlags, point);
+}
+
+
+void CWatchDialog::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialog::OnMouseMove(nFlags, point);
 }
